a0472184382656330aab23279c43e4dac3949e8c
ayman3tta <aymanatta71@gmail.com>
2026-09-25 12:50:09 +0300

    github_pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/SYSTEM1/',
   plugins: [
     react(),
     tailwindcss()
